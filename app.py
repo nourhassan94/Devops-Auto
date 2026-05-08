55dc9a22e0eed073ee1ff1238208ce316dea86de
nourhassan94 <nourhassan94@hotmail.com>
2026-05-08 12:25:02 +0200

    Added CI pipline

---
+++ app.py
@@ -0,0 +1,6 @@
+import utils
+
+print("Devops Autmation App")
+print(utils.add(2, 5))
+print(utils.multiply(5, 5))
+
---
+++ test_utils.py
@@ -0,0 +1,9 @@
+import utils
+
+def test_add():
+    assert utils.add(2, 3) == 5
+
+def test_multiply():
+    assert utils.multiply(5, 5) == 25
+
+    
